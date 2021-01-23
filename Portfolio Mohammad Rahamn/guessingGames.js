af80ce89597447d1ccbccf3b8d0ef6c137039cbe
Mohammad M Rahman <45700103+mabmahmud@users.noreply.github.com>
2021-01-23 02:49:39 -0400

    Add files via upload

---
+++ Portfolio Mohammad Rahamn/guessingGames.js
@@ -0,0 +1,41 @@
+/*
+Name: Mohammad Mahmudur Rahman
+Date: 05 February 201
+Purpose: To calculate taxMoney after taxing the given amount
+*/
+// MAin function to execute full program
+function numGuesser() {
+    var number = Math.floor(1+Math.random()*30);
+    console.log(number);
+    var guess;
+    let i =0;
+    for (i; i < 3; i++) {
+        guess = Number(prompt("Please enter your guessing number among 1 - 30 "));
+        if (isNaN(guess)) {
+            //return 'Invalid';
+            alert("You can guess again ")
+        }
+        if(number>guess){
+            alert("Number is too low");
+        }
+        if(number<guess){
+            alert("Number is too high");
+        }
+        if (number == guess) {
+            if(window.confirm("You won!! Would you like to play again??")){
+                numGuesser();
+            }else{
+                alert("Thank you for playing");
+                break;
+            }
+        }
+        if(i==2){
+            alert("Game is over");
+            break;
+        }
+        else {
+            guess = prompt("Please try again")
+        }
+    }
+}
+numGuesser();
---
+++ Portfolio Mohammad Rahamn/taxMoney.js
@@ -0,0 +1,48 @@
+/*
+Name: Mohammad Mahmudur Rahman
+Date: 05 February 201
+Purpose: To calculate taxMoney after taxing the given amount
+*/
+
+// To create calculateTax function
+function calculateTax(amount){
+    var total;  // Declaring a variable
+    //if statement to calculate tax
+    if(amount<=5.00){
+        total =amount +amount*(20/100);
+    }
+    if(amount>= 5.01 && amount<=100)
+    {
+        total =amount +amount*(15/100);
+    }
+    if(isNaN(amount)){
+        return 'Invalid';
+    }
+    else
+    {
+        total = amount +amount*(5/100);
+    }
+    return total.toFixed(2);
+}
+
+// To create formatMoney function
+function formatMoney(amount,flag){
+    let tempAmount = parseFloat(amount);
+    if(flag){
+        return "$"+tempAmount.toLocaleString();
+    }else{
+        return tempAmount;
+    }
+}
+// Prompt function to get the value from user
+var dolarAmount = prompt("Please enter dollar amount ");
+// To call the calculateTax function and store value in a variable
+let  totaqlAmount  = calculateTax(Number(dolarAmount));
+let temp = formatMoney(totaqlAmount,true);
+ console.log(temp);
+ //Use alert function to pop up result in dialogue box
+ alert (temp);
+
+
+
+
